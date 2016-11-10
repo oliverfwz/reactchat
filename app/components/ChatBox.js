800f733faf6e9fd7448bb08863ace802180f39f4
Oliver Futureworkz <oliver@futureworkz.com>
2016-11-10 11:49:42 +0700

    Add login with FB, twitter, github

--- app/components/ChatBox.js
+++ app/components/ChatBox.js
@@ -1,10 +1,67 @@
 import React from 'react'
 import Message from './Message'
 import AddMessageForm from './AddMessageForm'
+import firebase from 'firebase/app'
+import { auth } from '../db/firebase'
 
 class ChatBox extends React.Component {
+  constructor() {
+    super();
+    this.logout = this.logout.bind(this);
+    this.state = {
+      uid: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.authHandler({user})
+      }
+    });
+  }
+
+  authenticate(provider) {
+    console.log(`Trying to log in with ${provider}`);
+    auth.signInWithPopup(provider).then((authData) => this.authHandler(authData))
+  }
+
+  authHandler(authData) {
+    this.setState({
+      uid: authData.user.uid
+    });
+  }
+
+  logout() {
+    auth.signOut()
+    this.setState({ uid: null });
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Chat with everybody</h2>
+        <p>Sign in to continue!</p>
+        <div>
+          <button className="github" onClick={() => this.authenticate(new firebase.auth.GithubAuthProvider())}>Log In with Github</button>
+        </div>
+        <div>
+          <button className="facebook" onClick={() => this.authenticate(new firebase.auth.FacebookAuthProvider())} >Log In with Facebook</button>
+        </div>
+        <div>
+          <button className="twitter" onClick={() => this.authenticate(new firebase.auth.TwitterAuthProvider())} >Log In with Twitter</button>
+        </div>
+      </nav>
+    )
+  }
+
   render() {
+    const logout = <button onClick={this.logout}>Log Out!</button>;
     const { messages, addMessage } = this.props
+
+    if(!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
     
     return (
       <div className="ChatBox">
@@ -16,6 +73,7 @@ class ChatBox extends React.Component {
           }
         </div>
         <AddMessageForm addMessage={addMessage} />
+        {logout}
       </div>
     )
   }
